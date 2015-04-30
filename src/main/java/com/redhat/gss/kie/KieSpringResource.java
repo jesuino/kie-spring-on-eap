21364e873d96c1f31a6c4940a7266cc6236fde87
jesuino <william.a.siqueira@gmail.com>
2015-04-30 03:08:09 -0300

    Adjusting a few things

--- src/main/java/com/redhat/gss/kie/KieSpringResource.java
+++ src/main/java/com/redhat/gss/kie/KieSpringResource.java
@@ -3,7 +3,6 @@ package com.redhat.gss.kie;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
-import org.kie.api.definition.KiePackage;
 import org.kie.api.runtime.KieSession;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -13,7 +12,6 @@ public class KieSpringResource {
 
 	@GET
 	public void test() {
-		System.out.println("Starting...");
 		ApplicationContext ctx = new ClassPathXmlApplicationContext(
 				"com/redhat/gss/kie/kmodule-spring.xml");
 		KieSession kieSession = (KieSession) ctx.getBean("kieSession");
